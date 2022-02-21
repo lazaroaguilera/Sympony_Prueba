568d8b410f1f27366354f12329e127da87a620d9
lazaroaguilera <lazaro_aguilera@hotmail.com>
2022-02-21 12:46:35 +0100

    Modificación de router.yaml(Linea 9)

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -3,11 +3,23 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class QuestionController
 {
+    /**
+     * @Route("/")
+     */
+
     public function homepage(){
 
         return new Response("Pero qué controlador tan embrujado hemos conjurado!");
+    }
+
+    /**
+     * @Route("/questions/how-to-tie-my-shoes-with-magic")
+     */
+    public function show(){
+
     }
 }
